5e254f2a28dfc0491a620d1193c5a587ee309129
Bob Hua <schua1013@gmail.com>
2023-10-03 21:13:37 -0400

    Refactor some of the code. Trying to define actor space and observation space. Haven't figure out how to skip user

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from thecrew import thecrew_env_v0
-from thecrew.env.agent import Player
+from pettingzoo.test import api_test
 
 env = thecrew_env_v0.env()
-# env.config()
-agents = [Player(name) for name in env.possible_agents]
+env.reset()
+api_test(env, num_cycles=1)
+for agent in env.agent_iter(10):
+    
+    pass
+
+env.close()
 # Interact with the environment
+
--- thecrew/env/thecrew_env.py
+++ thecrew/env/thecrew_env.py
@@ -1,22 +1,31 @@
 from pettingzoo import AECEnv
-from env.agent import Player
-# from env.card import Card
-# from env.card import PlayingCard, TaskCards
-import unittest
+
 import random
-from collections import Counter, defaultdict
+from collections import Counter, deque
+from gymnasium.spaces import Discrete, Dict, Sequence
+from itertools import cycle
+
+
 REWARD_MAP = {
     "win": 10,
     "lose": -10,
     "task_complete": 1,
 }
+
+
 def env(**kwargs):
     return raw_env(**kwargs)
 
 
 class raw_env(AECEnv):
     def __init__(
-        self, colors: int = 4, ranks: int = 9, rockets: int = 4, players: int = 3, tasks: int = 3
+        self,
+        seed: int,
+        colors: int = 4,
+        ranks: int = 9,
+        rockets: int = 4,
+        players: int = 3,
+        tasks: int = 3,
     ):
         self.__config = {
             "colors": colors,
@@ -25,75 +34,93 @@ class raw_env(AECEnv):
             "players": players,
             "tasks": tasks,
         }
-        self.reset()
-
-    def reset(self):
         # assume play order is player_1 -> player_2 -> player_3 -> ... -> player_n -> player_1 ...
         self.possible_agents = [f"player_{i}" for i in range(self.__config["players"])]
+        self.num_agents = self.__config["players"]
         self.playing_cards, self.task_cards = self.__generateAllCards()
+
+    def __createActionSpaces(self):
+        return {
+            agent: Dict(
+                {
+                    # no info, top, mid, bottom
+                    "token": Discrete(4),
+                    # every turn agent can play a card in hand
+                    "card": Discrete(len(self.hands[agent])),
+                }
+            )
+            for agent in self.possible_agents
+        }
+
+    # def __createObservationSpaces(self):
+    # return {agent: Dict({"hand": }) for agent in self.possible_agents}
+
+    def reset(self, seed: int | None = None, options: dict | None = None) -> None:
+        random.seed(seed)
         random.shuffle(self.playing_cards)
-        random.shuffle(self.task_cards)        
-        self.hands = []
-        for _ in range(len(self.possible_agents)):
-            self.hands.append([])
-        self.tasks = []
-        for _ in range(len(self.possible_agents)):
-            self.tasks.append([])
-        self.commander = -1
-        start_idx = 0
+        random.shuffle(self.task_cards)
+        self.hands = {}
+        self.tasks = {}
+        self.suit_counters = {}
         self.task_dict = {}
-        
-        #count of cards in each suit for each player. Useful for checking legal plays
-        self.suit_counts = [Counter() for _ in range(len(self.possible_agents))]
-        for card in self.playing_cards:
-            if card[0] == "R" and card[1] == self.__config["rockets"]:
-                self.commander = start_idx
-            self.hands[start_idx].append(card)
-            self.suit_counts[start_idx][card[0]] += 1
-            start_idx = (start_idx + 1) % len(self.possible_agents)
+        self.commander = None
+
+        for player in range(len(self.possible_agents)):
+            self.hands[player] = []
+            self.tasks[player] = []
+            # count of cards in each suit for each player. Useful for checking legal plays
+            self.suit_counters[player] = Counter()
+
+        self.__dealPlayingCards()
+        self.__assignCommander()
+        self.__dealTaskCards()
         for hand in self.hands:
             hand.sort()
-            
-        if self.__config["rockets"] == 0:
-            self.commander = random.randint(0, len(self.possible_agents) - 1)
 
-        start_idx = self.commander
-        for _ in range(self.__config["tasks"]):
-            task = self.task_cards.pop(0)
-            self.tasks[start_idx].append(task)
-            self.task_dict[task] = start_idx
-            start_idx = (start_idx + 1) % len(self.possible_agents)
         self.player_to_play = self.commander
-        self.current_trick: list[tuple[Player, tuple[str,int]]] = [] 
-        
-    # Perfect information function. All hands returned. 
+        self.current_trick: list[tuple[str, tuple[str, int]]] = []
+
+    # Perfect information function. All hands returned.
     # Will need to be updated to return only the hand of the current player, as well as belief distribution over the other hands (?).
     # Also, note that we'll need to track the previously played cards as well.
     def observe(self, agent):
-        return self.hands, self.tasks, self.current_trick, self.commander, self.suit_counts, self.player_to_play, self.task_dict
-
+        return (
+            self.hands,
+            self.tasks,
+            self.current_trick,
+            self.commander,
+            self.suit_counts,
+            self.player_to_play,
+            self.task_dict,
+        )
 
     # for now, action is just the index of the card to play. Hints will be added later.
     # Return value is false if the game is over, true otherwise.
     def step(self, action: int) -> int:
-        '''
+        """
         action: index of card to play
-        
+
         Returns:
             int: 1 if win, -1 if loss, 0 otherwise
-        '''
-        if action >= len(self.hands[self.player_to_play]):
+        """
+        if action >= len(self.hands[self.agent_selection]):
             raise ValueError(f"Invalid action, value {action} out of range.")
-        
-        card = self.hands[self.player_to_play].pop(action)
-        self.suit_counts[self.player_to_play][card[0]] -= 1
-    
+
+        card = self.hands[self.agent_selection].pop(action)
+        self.suit_counters[self.agent_selection][card[0]] -= 1
+
         # check if card playable in current trick
         if len(self.current_trick) > 0:
-            if self.current_trick[0][1][0] != card[0] and self.suit_counts[self.player_to_play][self.current_trick[0][1][0]] > 0:
-                raise ValueError(f"Invalid action, card {card} not playable. Trick so far: {self.current_trick}")
+            if (
+                self.current_trick[0][1][0] != card[0]
+                and self.suit_counts[self.agent_selection][self.current_trick[0][1][0]]
+                > 0
+            ):
+                raise ValueError(
+                    f"Invalid action, card {card} not playable. Trick so far: {self.current_trick}"
+                )
 
-        self.current_trick.append((self.player_to_play, card))
+        self.current_trick.append((self.agent_selection, card))
 
         # check if trick is over
         if len(self.current_trick) == len(self.possible_agents):
@@ -108,7 +135,7 @@ class raw_env(AECEnv):
                     trick_value = card[1]
                     trick_suit = "R"
                     winner = player
-            
+
             # check tasks
             for _, card in self.current_trick:
                 if card in self.task_dict:
@@ -123,7 +150,9 @@ class raw_env(AECEnv):
                     else:
                         # TODO: insert code to update reward negatively
                         return -1
-            self.player_to_play = winner
+            
+            self.agent_selection = winner
+            
             self.current_trick = []
         else:
             self.player_to_play = (self.player_to_play + 1) % len(self.possible_agents)
@@ -131,11 +160,11 @@ class raw_env(AECEnv):
 
     def render(self):
         s = f"config: {self.__config} \n"
-        s+= f"hands: {self.hands} \n"
-        s+= f"tasks: {self.task_dict} \n"
-        s+= f"commander: {self.commander}\n"
-        s+= f"current_trick: {self.current_trick}\n"
-        s+= f"current_turn: {self.player_to_play}\n"
+        s += f"hands: {self.hands} \n"
+        s += f"tasks: {self.task_dict} \n"
+        s += f"commander: {self.commander}\n"
+        s += f"current_trick: {self.current_trick}\n"
+        s += f"current_turn: {self.player_to_play}\n"
         return s
 
     def config(self):
@@ -146,13 +175,39 @@ class raw_env(AECEnv):
 
     def __generateAllCards(self):
         playing_cards = []
-        task_cards = []
+        task_cards = deque()
         # Color Cards & Task Cards
-        for color in ["B", "P", "G", "Y"][: self.__config["colors"]]:
+        for suit in ["B", "P", "G", "Y"][: self.__config["colors"]]:
             for rank in range(1, self.__config["ranks"] + 1):
-                playing_cards.append((color, rank))
-                task_cards.append((color, rank))
+                playing_cards.append((suit, rank))
+                task_cards.append((suit, rank))
         # Rocket Cards
         for value in range(1, self.__config["rockets"] + 1):
             playing_cards.append(("R", value))
         return playing_cards, task_cards
+
+    def __dealPlayingCards(self):
+        player_cycle = cycle(self.possible_agents)
+        for card in self.playing_cards:
+            player = next(player_cycle)
+            self.hands[player].append(card)
+            self.suit_counters[player][card[0]] += 1
+
+    def __assignCommander(self):
+        if self.__config["rockets"] == 0:
+            self.commander = random.choice(self.possible_agents)
+        else:
+            for player in self.possible_agents:
+                if ("R", self.__config["rockets"]) in self.hands[player]:
+                    self.commander = player
+                    break
+
+    # Random for now
+    def __dealTaskCards(self):
+        player_cycle = cycle(self.possible_agents)
+        for _ in range(self.possible_agents.index(self.commander)):
+            next(player_cycle)
+        for _ in range(self.__config["tasks"]):
+            player = next(player_cycle)
+            self.tasks[player].append(task := self.task_cards.pop(0))
+            self.task_dict[task] = player
