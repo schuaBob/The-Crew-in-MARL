c509e263e3f9179f9ad3418982ae6668a1895152
Bob Hua <schua1013@gmail.com>
2023-10-04 23:25:24 -0400

    delete unnecessary files

--- thecrew/env/agent.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    def __init__(self, name):
-        self.__name = name
-        self.__hand = []
-        self.__isCommander = False
-
-    def __repr__(self):
-        return f"{self.__name}"
